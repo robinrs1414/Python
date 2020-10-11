9b2b5bf5083b959f791b86a3e8cd259f0a563a4b
Robin <57723664+robinrs1414@users.noreply.github.com>
2020-10-11 07:59:42 +0530

    Add files via upload

---
+++ Batter Percentage Notification/batterypercentage.py
@@ -0,0 +1,10 @@
+import psutil
+from plyer import notification
+import time
+
+battery = psutil.sensors_battery()
+while(True):
+    percent = battery.percent
+    notification.notify(title="Battery Percentage", message=str(percent)+'% Battery Remaining', timeout=10)
+    time.sleep(60*60)
+    continue
